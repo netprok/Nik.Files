63b23d138a98aab62d6c8eb1ab8fa345d015e3a5
Ehsan Faramarzi <netpro@email.com>
2024-02-01 22:10:42 +0100

    using using!

--- Nik.Files/BinaryFileWriter.cs
+++ Nik.Files/BinaryFileWriter.cs
@@ -4,8 +4,8 @@ public sealed class BinaryFileWriter : IBinaryFileWriter
 {
     public async Task WriteContentAsync(string fileName, byte[] content)
     {
-        FileStream fileStream = new(fileName, FileMode.Create);
-        MemoryStream memoryStream = new(content);
+        using FileStream fileStream = new(fileName, FileMode.Create);
+        using MemoryStream memoryStream = new(content);
         await memoryStream!.CopyToAsync(fileStream);
     }
 }
